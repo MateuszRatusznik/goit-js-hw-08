54a3bee76baf75a4d1bb854f38f08749d7eccc5c
Mateusz <ratusznikmateusz@gmail.com>
2022-07-28 14:26:46 +0200

    deployment

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
+// Opisany w dokumentacji
+import SimpleLightbox from 'simplelightbox';
+// Dodatkowy import stylów
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+
+const galleryItem = galleryItems.map((galleryItem) => `
+<a class="gallery__item" href="${galleryItem.original}">
+  <img class="gallery__image" src="${galleryItem.preview}" 
+  alt="${galleryItem.description}" title = "${galleryItem.description}"/>
+</a>`).join("");
+
+gallery.innerHTML = galleryItem;
+
+new SimpleLightbox('.gallery a', { captionDelay: 250 });
 
 console.log(galleryItems);
