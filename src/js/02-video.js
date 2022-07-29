50094816406c2a1f704a7a6178e115838a5136d3
Mateusz <ratusznikmateusz@gmail.com>
2022-07-29 11:21:04 +0200

    zad 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-player.on('play', function() {
-    console.log('played the video!');
+
+    player.on('timeupdate', throttle(({duration, percent, seconds}) => {
+        localStorage.setItem("videoplayer-current-time", `${seconds}`);
+    }, 1000));
+
+const actualTime = localStorage.getItem("videoplayer-current-time"); 
+
+player.setCurrentTime(actualTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
 });
-
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
-});
-
-onPlay.addEventListener() {
-    // data is an object containing properties specific to that event
-};
-player.on('timeupdate', function(data) {
-    // data is an object containing properties specific to that event
-});
-
-player.on('play', onPlay);
