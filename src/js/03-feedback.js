47bdbd86beb7b5b01942355e59c317c06c671d2c
Mateusz <ratusznikmateusz@gmail.com>
2022-07-30 16:15:56 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,6 +5,7 @@ const input = document.querySelector("input");
 const message = document.querySelector("textarea")
 const localStorageKey = "feedback-form-state";
 
+
 form.addEventListener("input", throttle((event) => {
     const inputData = {email: form.email.value, message: form.message.value};
     localStorage.setItem(localStorageKey, JSON.stringify(inputData));
@@ -14,7 +15,7 @@ const storageData = localStorage.getItem(localStorageKey);
 const parseData = JSON.parse(storageData);
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault();//
+    event.preventDefault();
     const { elements: { email, message }} = event.currentTarget;
     console.log({email: email.value, message: message.value});
     event.currentTarget.reset();
