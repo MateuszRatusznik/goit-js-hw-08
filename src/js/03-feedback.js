dedf6cab636aa6e02512a2f9df4efa3e12660e9c
Mateusz <ratusznikmateusz@gmail.com>
2022-07-30 16:11:50 +0200

    kurwa

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
 import throttle from "lodash.throttle";
+
+const form = document.querySelector(".feedback-form");
+const input = document.querySelector("input");
+const message = document.querySelector("textarea")
+const localStorageKey = "feedback-form-state";
+
+form.addEventListener("input", throttle((event) => {
+    const inputData = {email: form.email.value, message: form.message.value};
+    localStorage.setItem(localStorageKey, JSON.stringify(inputData));
+}, 500));
+
+const storageData = localStorage.getItem(localStorageKey);
+const parseData = JSON.parse(storageData);
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();//
+    const { elements: { email, message }} = event.currentTarget;
+    console.log({email: email.value, message: message.value});
+    event.currentTarget.reset();
+    localStorage.clear();
+  });
+
+const resetForm = () => {
+    if(parseData !== "") {
+        input.value = parseData.email;
+        message.value = parseData.message;
+    }
+}
+
+resetForm();
