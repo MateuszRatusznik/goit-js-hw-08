5ff49c1d5a33955dcec975784313af547da6b7cb
Mateusz <ratusznikmateusz@gmail.com>
2022-07-31 12:39:21 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,6 @@
 // Opisany w dokumentacji
 import SimpleLightbox from 'simplelightbox';
-// Dodatkowy import stylów
+// Dodatkowy iMpoRt stylów
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items.js';
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -16,7 +16,7 @@ player.setCurrentTime(actualTime).then(function(seconds) {
 }).catch(function(error) {
     switch (error.name) {
         case 'RangeError':
-            // the time was mr less than 0 or greater than the video’s duration
+            // the time was MR less than 0 or greater than the video’s duration
             break;
 
         default:
